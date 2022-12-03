66556a38217bff9d2832a85ed2f81677b21db995
mobius29 <sagjin0000@naver.com>
2022-12-03 16:07:00 +0900

    [FTR]: emotion을 이용한 global CSS 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { Global } from '@emotion/react';
+import { globalStyle } from './styles/globalStyle';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
+    <Global styles={globalStyle} />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
